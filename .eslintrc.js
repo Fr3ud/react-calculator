f494c4acdadcd05f799351d0a39e4833022d9eaa
Fr3ud <oo.pavelko@gmail.com>
2021-03-27 16:55:48 +0200

    feat: add App component

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['prettier'],
-  parser: 'babel-eslint',
-  plugins: ['prettier'],
-  rules: {
-    quotes: [2, 'single', { avoidEscape: true }],
-    'jsx-quotes': [2, 'prefer-double'],
-  },
-};
---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+export default class extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Hello World!</p>
+      </div>
+    );
+  }
+}
