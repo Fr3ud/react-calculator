280e15a2b86d93da7c91c54aa36db19d7616a4f0
Fr3ud <oo.pavelko@gmail.com>
2021-03-27 15:43:14 +0200

    feat: update settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
-  extends: ["prettier"],
-  parser: "babel-eslint",
-  plugins: ["prettier"],
+  extends: ['prettier'],
+  parser: 'babel-eslint',
+  plugins: ['prettier'],
   rules: {
-    "prettier/prettier": ["error"],
+    quotes: [2, 'single', { avoidEscape: true }],
+    'jsx-quotes': [2, 'prefer-double'],
   },
 };
